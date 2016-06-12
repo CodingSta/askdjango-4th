47d9dd1c031ae7fd4e274eb5082c08fa30a14ba7
Chinseok Lee <allieuslee@gmail.com>
2016-06-12 15:53:21 +0900

    blog.views.index2 뷰를 TemplateView 를 통해 생성

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import include, url
 
-# blog/api/v1.py
-# blog/api/v2.py
-
 urlpatterns = [
     url(r'^$', 'blog.views.index'),
+    url(r'^index2/$', 'blog.views.index2'),
     url(r'^1/$', 'blog.views.post_detail'),
     url(r'^api/v1/', include('blog.api.v1')),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
 def index(request):
     return render(request, 'blog/index.html')
+
+
+index2 = TemplateView.as_view(template_name='blog/index.html')
 
 
 def post_detail(request):
