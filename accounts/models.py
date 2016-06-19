c70f84b6b88d3247120177408d5d45ba59b76fd5
Chinseok Lee <allieuslee@gmail.com>
2016-06-19 12:11:06 +0900

    accounts.Profile 모델 phone 필드에 phone number validator 적용

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,15 @@
+import re
 from django.conf import settings
+from django.forms import ValidationError
 from django.db import models
+
+
+def phonenumber_validator(value):
+    if re.match(r'^01[016789][1-9]\d{6,7}$', value) is None:
+        raise ValidationError('휴대폰 번호를 입력해주세요.')
 
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
     address = models.CharField(max_length=100, blank=True)
-    phone = models.CharField(max_length=15, blank=True)
+    phone = models.CharField(max_length=15, blank=True, validators=[phonenumber_validator])
