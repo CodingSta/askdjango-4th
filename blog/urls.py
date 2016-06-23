e13d20d107de7d19c81a233fa9c1b9ffea1db23a
Chinseok Lee <allieuslee@gmail.com>
2016-06-23 21:14:15 +0900

    많이 꼰 url reverse 예시

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import include, url
 urlpatterns = [
     url(r'^$', 'blog.views.index'),
     url(r'^index2/$', 'blog.views.index2'),
-    url(r'^1/$', 'blog.views.post_detail'),
+    url(r'^(?P<category_pk>\d+)/(?P<pk>\d+)/$', 'blog.views.post_detail'),
+    url(r'^(?P<pk>\d+)/$', 'blog.views.post_detail'),
+    url(r'^(?P<pk>\w+)/$', 'blog.views.post_detail'),
     url(r'^api/v1/', include('blog.api.v1')),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 def index(request):
-    return render(request, 'blog/index.html')
+    return render(request, 'blog/index.jinja2')
 
 
 index2 = TemplateView.as_view(template_name='blog/index.html')
 
 
-def post_detail(request):
-    pass
+def post_detail(request, pk, category_pk=None):
+    return HttpResponse('pk = {}, category_pk = {}'.format(pk, category_pk))
