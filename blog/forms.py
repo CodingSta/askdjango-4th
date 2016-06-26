2a56e40e8216065c15060e00e5940ef1e3f5a589
Chinseok Lee <allieuslee@gmail.com>
2016-06-26 11:22:37 +0900

    PostForm 이 PostModelForm 과 같은 인터페이스를 가지도록 save() 를 구현

--- blog/forms.py
+++ blog/forms.py
@@ -7,6 +7,13 @@ class PostForm(forms.Form):
     title = forms.CharField(validators=[MinLengthValidator(3)])
     content = forms.CharField(widget=forms.Textarea)
 
+    # forms.Form 에는 없지만, ModelForm 과 유사한 인터페이스를 만들어주기 위해 구현
+    def save(self):
+        title = self.cleaned_data['title']
+        content = self.cleaned_data['content']
+        post = Post.objects.create(title=title, content=content)
+        return post
+
 
 class PostModelForm(forms.ModelForm):
     class Meta:
