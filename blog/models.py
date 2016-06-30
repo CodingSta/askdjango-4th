1c65ed86c85c7de47b34290fd729fd452a0dd39d
Chinseok Lee <allieuslee@gmail.com>
2016-06-30 20:09:15 +0900

    blog.models.Post 에 대해 pre_save 시그널 등록

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,14 @@
 from django.core.urlresolvers import reverse
 from django.core.validators import MinLengthValidator
 from django.db import models
+from django.db.models.signals import pre_save
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100, validators=[MinLengthValidator(3)])
     content = models.TextField()
     photo = models.ImageField()
+    thumbnail = models.ImageField()
     tags = models.ManyToManyField('Tag', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -22,6 +24,15 @@ class Post(models.Model):
             'tags': self.tags.all(),
         }
 
+    # https://docs.djangoproject.com/en/1.9/ref/signals/#django.db.models.signals.post_save
+    @staticmethod
+    def on_pre_save(sender, **kwargs):
+        # if kwargs['created']: pass
+        post = kwargs['instance']
+        # post.photo
+        # post.thumbnail
+
+pre_save.connect(Post.on_pre_save, sender=Post)
 
 
 class Comment(models.Model):
