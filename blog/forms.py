1d10eb3e9aade72cc87ad57e7686a6211aaaacf1
Chinseok Lee <allieuslee@gmail.com>
2016-06-30 20:01:56 +0900

    blog.forms.PostModelForm 에 clean 멤버함수를 통한 validator 적용

--- blog/forms.py
+++ blog/forms.py
@@ -21,3 +21,20 @@ class PostModelForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'photo']  #'__all__'
+
+    '''
+    def clean_title(self):
+        title = self.cleaned_data.get('title', None)
+        if title:
+            title = title.strip()
+            if len(title) < 10:
+                raise forms.ValidationError('10자 이상 입력하세요.')
+        return title
+
+    def clean(self):
+        title = self.cleaned_data['title']
+        content = self.cleaned_data['content']
+        if len(title) + len(content) >= 10:
+            raise forms.ValidationError('제목과 내용. 길이의 합이 10보다 작게 입력해주세요.')
+        return self.cleaned_data
+    '''
