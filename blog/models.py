d90bec734c0770879e77cff294330db843b6fc69
Chinseok Lee <allieuslee@gmail.com>
2016-06-30 21:49:16 +0900

    잘못 지정된 blog.models.Post.thumbnail 필드 제거

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,6 @@ class Post(models.Model):
     title = models.CharField(max_length=100, validators=[MinLengthValidator(3)])
     content = models.TextField()
     photo = models.ImageField()
-    thumbnail = models.ImageField()
     tags = models.ManyToManyField('Tag', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
