1870def3e9c538be425048dfd03223e1732ff7a1
Chinseok Lee <allieuslee@gmail.com>
2016-07-03 13:24:10 +0900

    askdjango.settings.prod 내 STATIC/MEDIA/SECRET_KEY 설정 추가

--- askdjango/settings/prod.py
+++ askdjango/settings/prod.py
@@ -4,6 +4,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "staticfiles")
+MEDIA_ROOT = os.path.join(BASE_DIR, "..", "media")
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', SECRET_KEY)
+
 INSTALLED_APPS += [
     # '',
 ]
