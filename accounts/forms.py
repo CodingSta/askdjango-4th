9d75838084e0118d63d7252fea577b30d5dae8c9
Chinseok Lee <allieuslee@gmail.com>
2016-07-03 15:11:49 +0900

    회원가입 폼에서 address/phone 추가 입력 처리

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,16 +1,23 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
+from accounts.models import Profile
 
 
 class SignupForm(UserCreationForm):
     username = forms.EmailField(label='Email')
+    address = forms.CharField(required=False)
+    phone = forms.CharField(required=False)
 
-    def save(self, commit=True):
+    def save(self):
         user = super(SignupForm, self).save(commit=False)
         user.email = user.username
-        if commit:
-            user.save()
+        user.save()
+
+        address = self.cleaned_data.get('address', '')
+        phone = self.cleaned_data.get('phone', '')
+        Profile.objects.create(user=user, address=address, phone=phone)
+
         return user
 
 
