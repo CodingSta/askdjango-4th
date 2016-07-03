e2275444952b9b57cefb96cca589f025f32d0348
Chinseok Lee <allieuslee@gmail.com>
2016-07-03 11:32:56 +0900

    askdjango.settings.prod 배포준비 완료

--- askdjango/settings/prod.py
+++ askdjango/settings/prod.py
@@ -7,3 +7,13 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS += [
     # '',
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ubuntu',
+        'USER': 'ubuntu',
+        'PASSWORD': 'withaskdjango!',
+        'HOST': '127.0.0.1',
+    }
+}
